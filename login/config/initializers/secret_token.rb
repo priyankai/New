ec75b29767bfbeca8588c9971063d79c0678e77f
Priyanka Ivatury <priyanka.ivatury@gmail.com>
2012-09-07 22:42:18 +0530

    added ruby files

---
+++ login/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Login::Application.config.secret_token = 'b995fedd26b970794ddde28c417d598f03dc7bb7c13c11c6919e87f380d983c565c8b177c45887a75a05e95702bf9470c22a48fdc44bbc46744313b0f422d923'
